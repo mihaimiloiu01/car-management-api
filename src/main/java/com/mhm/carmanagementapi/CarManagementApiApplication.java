68252290564a72ec2cc157f332126f144ea3a8bc
Mihai Miloiu <mmiloiu@deloittece.com>
2023-08-08 17:58:45 +0300

    Initial Commit

---
+++ src/main/java/com/mhm/carmanagementapi/CarManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.mhm.carmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementApiApplication.class, args);
+	}
+
+}
